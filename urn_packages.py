12ce3d3fcf04c8e60ef306ebaba2a69a6d17d10d
Samuel Forbes <saf92222@yahoo.co.uk>
2023-08-29 18:03:08 +0100

    Delete urn_packages.py

--- urn_packages.py
+++
@@ -1,8 +0,0 @@
-#Imported packages
-
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy.optimize import fsolve
-from scipy.stats import expon
-from tqdm import tqdm
